1adad45a692c03c491b0603d0503a0729b3ce3b4
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-06 00:05:58 +0200

    started the homework project

---
+++ StoreBalance/src/test/java/com/github/shmvanhouten/StoreBalanceTest.java
@@ -0,0 +1,14 @@
+package com.github.shmvanhouten;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class StoreBalanceTest {
+    @Test
+    public void itShouldDoSomething() throws Exception{
+
+    }
+}
