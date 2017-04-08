3292858d1cd24af5e7a863af3231eb857e6701c5
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-08 21:35:41 +0200

    Using BigDecimal to represent our prices. Made Map pricelist in the PriceList class, made method inputPrice add the value to a key or create the key- value pair if the key was not yet there.

--- StoreBalance/src/main/java/com/github/shmvanhouten/PriceList.java
+++ StoreBalance/src/main/java/com/github/shmvanhouten/PriceList.java
@@ -1,12 +1,16 @@
 package com.github.shmvanhouten;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PriceList {
-
-    public void inputPrice(String productName, double price) {
-
+    public Map<String, BigDecimal> priceList =  new HashMap<>();
+    public void inputPrice(String productName, BigDecimal price) {
+        priceList.put(productName, price);
     }
 
-    public double getPrice(String productName) {
-        return 2.50;
+    public BigDecimal getPrice(String productName) {
+        return priceList.get(productName);
     }
 }
--- StoreBalance/src/test/java/com/github/shmvanhouten/PriceListTest.java
+++ StoreBalance/src/test/java/com/github/shmvanhouten/PriceListTest.java
@@ -1,6 +1,9 @@
 package com.github.shmvanhouten;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -9,7 +12,24 @@ public class PriceListTest {
     @Test
     public void itShouldAddThePriceOfTomatoSoup () throws Exception{
         PriceList priceList = new PriceList();
-        priceList.inputPrice("tomatoSoup", 2.50);
-        assertThat(priceList.getPrice("tomatoSoup"), is(2.50));
+        BigDecimal tomatoSoupPrice = new BigDecimal("2.50");
+        priceList.inputPrice("tomatoSoup", tomatoSoupPrice);
+        assertThat(priceList.getPrice("tomatoSoup"), is(tomatoSoupPrice));
+    }
+    @Test
+    public void itShouldAddADifferentPriceOfTomatoSoup () throws Exception{
+        PriceList priceList = new PriceList();
+        BigDecimal tomatoSoupPrice = new BigDecimal("2.30");
+        priceList.inputPrice("tomatoSoup", tomatoSoupPrice);
+        assertThat(priceList.getPrice("tomatoSoup"), is(tomatoSoupPrice));
+    }
+    @Test
+    public void itShouldReturnTheSecondAddedPriceForTomatoSoup() throws Exception{
+        PriceList priceList = new PriceList();
+        BigDecimal tomatoSoupPrice = new BigDecimal("2.30");
+        BigDecimal newTomatoSoupPrice = new BigDecimal("2.25");
+        priceList.inputPrice("tomatoSoup", tomatoSoupPrice);
+        priceList.inputPrice("tomatoSoup", newTomatoSoupPrice);
+        assertThat(priceList.getPrice("tomatoSoup"), is(newTomatoSoupPrice));
     }
 }
