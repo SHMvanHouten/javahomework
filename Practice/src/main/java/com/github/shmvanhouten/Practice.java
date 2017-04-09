4100dead1acc8236c071230ef0fb693f87cdfcd4
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-09 15:50:51 +0200

    completed a few simple practice katas

--- Practice/src/main/java/com/github/shmvanhouten/Practice.java
+++ Practice/src/main/java/com/github/shmvanhouten/Practice.java
@@ -1,9 +1,41 @@
 package com.github.shmvanhouten;
 
+
 public class Practice {
     public String getPositionInAlphabet(char alphabet) {
         Character referenceChar = '`';
         int position = Math.abs(referenceChar.compareTo(alphabet));
         return "Position of alphabet: "+ position;
+    }
+
+    public double roundToTwoDecimalPlaces(double number) {
+        return Math.round(number*100d)/100d; //is inaccurate!
+//        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+//        return Double.parseDouble(decimalFormat.format(number)); is more accurate but because of Local configuration uses a ',' for a decimal point instead of a '.'
+    }
+
+    public char changeUpperCaseToLowerCase(char character) {
+        return Character.toLowerCase(character);
+    }
+
+    public String changeAllLettersToOppositeCase(String inputString) {
+        StringBuilder outputString = new StringBuilder();
+//        for(int i = 0; i<inputString.length(); i++){
+//            char letter = inputString.charAt(i);
+//            if(Character.isUpperCase(letter)){
+//                outputString += Character.toLowerCase(letter);
+//            }else{
+//                outputString += Character.toUpperCase(letter);
+//            }
+//
+//        }
+        for (char letter:inputString.toCharArray()) {
+            if(Character.isUpperCase(letter)){
+                outputString.append(Character.toLowerCase(letter));
+            }else{
+                outputString.append(Character.toUpperCase(letter));
+            }
+        }
+        return outputString.toString();
     }
 }
--- Practice/src/test/java/com/github/shmvanhouten/PracticeTest.java
+++ Practice/src/test/java/com/github/shmvanhouten/PracticeTest.java
@@ -1,6 +1,9 @@
 package com.github.shmvanhouten;
 
 import org.junit.Test;
+
+import java.text.DecimalFormat;
+
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
@@ -10,5 +13,26 @@ public class PracticeTest {
         Practice test = new Practice();
         assertThat(test.getPositionInAlphabet('a'), is("Position of alphabet: 1"));
         System.out.println("hi" + 1 + 1);
+        System.out.println((int)'t');
+    }
+
+    @Test
+    public void itShouldRoundTheDoubleToTwoDecimalPoints() throws Exception{
+        Practice test = new Practice();
+        assertThat(test.roundToTwoDecimalPlaces(5.5589), is(5.56));
+        assertThat(test.roundToTwoDecimalPlaces(3.3424), is(3.34));
+        DecimalFormat decimalFormat =  new DecimalFormat("#.##");
+        System.out.println(decimalFormat.format(5.5589));
+    }
+
+    @Test
+    public void itShouldTurnAnUpperCaseCharIntoALowerCase() throws Exception{
+        Practice test = new Practice();
+        assertThat(test.changeUpperCaseToLowerCase('A'), is('a'));
+    }
+    @Test
+    public void itShouldTurnUpperCaseLettersToLowerCaseAndViceVersa() throws Exception{
+        Practice test = new Practice();
+        assertThat(test.changeAllLettersToOppositeCase("HeLlo"), is("hElLO"));
     }
 }
