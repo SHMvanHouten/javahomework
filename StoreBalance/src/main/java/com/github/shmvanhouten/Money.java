c6a1a5cb895ccc21f46d17fef4598971be40b3ba
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-11 13:41:01 +0200

    added toString override and hashcode

--- StoreBalance/src/main/java/com/github/shmvanhouten/Money.java
+++ StoreBalance/src/main/java/com/github/shmvanhouten/Money.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class Money {
     private BigDecimal amount;
     private String currency;
-    Money(double amountInput, String currencyInput){
+    Money(String amountInput, String currencyInput){
         amount = new BigDecimal(amountInput);
         currency = currencyInput;
     }
@@ -15,11 +15,29 @@ public class Money {
     }
 
     public Money add(Money amountToAdd) {
-        return new Money(amount.add(amountToAdd.getAmount()).doubleValue(), currency);
+        return new Money(amount.add(amountToAdd.getAmount()).toString(), currency);
     }
 
     @Override
     public String toString() {
-        return
+        return amount.toString().concat(currency);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
+
+        if (amount != null ? !amount.equals(money.amount) : money.amount != null) return false;
+        return currency != null ? currency.equals(money.currency) : money.currency == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = amount != null ? amount.hashCode() : 0;
+        result = 31 * result + (currency != null ? currency.hashCode() : 0);
+        return result;
     }
 }
--- StoreBalance/src/test/java/com/github/shmvanhouten/MoneyTest.java
+++ StoreBalance/src/test/java/com/github/shmvanhouten/MoneyTest.java
@@ -10,17 +10,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class MoneyTest {
     @Test
     public void itShouldAcceptADecimalNumberAndACurrency() throws Exception{
-        Money tomatoSoupPrice = new Money(3.23, "eur");
-        Money colaPrice = new Money(1.75, "eur");
-        assertThat(tomatoSoupPrice.getAmount(), is(new BigDecimal(3.23)));
+        Money tomatoSoupPrice = new Money("3.23" , "eur");
+        Money colaPrice = new Money("1.75", "eur");
+        assertThat(tomatoSoupPrice.getAmount(), is(new BigDecimal("3.23")));
         BigDecimal mfmf = new BigDecimal("32");
         System.out.println(mfmf.doubleValue());
     }
 
     @Test
     public void itShouldAddTwoMoneyAmountsTogether() throws Exception{
-        Money tomatoSoupPrice = new Money(3.23, "eur");
-        Money colaPrice = new Money(1.75, "eur");
-        assertThat(tomatoSoupPrice.add(colaPrice), is(new Money(4.98, "eur")));
+        Money tomatoSoupPrice = new Money("3.23", "eur");
+        Money colaPrice = new Money("1.75", "eur");
+        assertThat(tomatoSoupPrice.add(colaPrice), is(new Money("4.98", "eur")));
     }
 }
