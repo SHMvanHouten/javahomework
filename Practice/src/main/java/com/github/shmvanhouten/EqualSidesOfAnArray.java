3c98ce0f7508f7c5e3d1027f89d3f0f9a1409b5b
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-11 16:02:00 +0200

    finished a small kata where you had to find the index of the arrayelement where the sum of the elements on each side add up to the same amount

---
+++ Practice/src/main/java/com/github/shmvanhouten/EqualSidesOfAnArray.java
@@ -0,0 +1,18 @@
+package com.github.shmvanhouten;
+
+import java.util.Arrays;
+
+public class EqualSidesOfAnArray {
+    public int getIndexWhereBothSidesOfTheIndexAddUpToTheSameAmount(int[] inputArray) {
+        int leftSide = inputArray[0];
+        int rightSide = Arrays.stream(inputArray).sum() - leftSide;
+        for(int i = 1; i<inputArray.length-1;i++){
+            rightSide -= inputArray[i];
+            if(leftSide == rightSide){
+                return i;
+            }
+            leftSide += inputArray[i];
+        }
+        return -1;
+    }
+}
