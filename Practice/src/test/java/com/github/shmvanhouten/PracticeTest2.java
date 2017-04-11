8e2c44129b9d6665bb63625aaa60d20d20f45285
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-11 13:55:10 +0200

    made a method that will change the value of a binary value to a decimal value so far for just 0 and 1

--- Practice/src/test/java/com/github/shmvanhouten/PracticeTest2.java
+++ Practice/src/test/java/com/github/shmvanhouten/PracticeTest2.java
@@ -1,20 +1,24 @@
 package com.github.shmvanhouten;
 
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PracticeTest2 {
     @Test
-    public void itShouldTurnANonCapitalLetterIntoCapital() throws Exception{
-        Practice2 practice = new Practice2();
-        assertThat(practice.changeTheStringIntoTheOddStringThingy("a"), is("A"));
-    }
+    public void itShouldReturnTheValue0for0and1for1() throws Exception{
+        Practice2 practice2 = new Practice2();
+        List<Integer> binary = new ArrayList<>(Arrays.asList(0));
 
-    @Test
-    public void itShouldTurnTheSecondLetterInTheStringToAnUpperCaseAndLowerCase() throws Exception{
-        Practice2 practice = new Practice2();
-        assertThat(practice.changeTheStringIntoTheOddStringThingy("ab"), is("A-Bb"));
-        assertThat(practice.changeTheStringIntoTheOddStringThingy("abc"), is("A-Bb-Ccc"));
+        assertThat(practice2.getDecimalValue(binary), is(0));
+        binary.clear();
+        binary.add(1);
+        assertThat(practice2.getDecimalValue(binary), is(1));
     }
 }
