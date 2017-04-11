60a518dddac2a029f7a366c241928da8a245de2d
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-04-11 18:57:33 +0200

    finished tortoise race kata.

--- Practice/src/test/java/com/github/shmvanhouten/TortoiseRaceTest.java
+++ Practice/src/test/java/com/github/shmvanhouten/TortoiseRaceTest.java
@@ -9,7 +9,22 @@ public class TortoiseRaceTest{
     @Test
     public void itShouldCalculateTheAmountOfSecondsItWouldTakeForTortoiseAToCatchUpWithTortoiseB() throws Exception {
         TortoiseRace race = new TortoiseRace();
-        assertThat(race.getSecondsNeededToCatchUp(20,40,20), is(360));
+        assertThat(race.getSecondsNeededToCatchUp(20,40,20), is(3600));
+    }
+
+    @Test
+    public void itShouldTurnSecondsIntoHoursMinutesAndSeconds() throws Exception{
+        TortoiseRace race = new TortoiseRace();
+        assertThat(race.convertSecondsIntoHoursMinutesAndSeconds(3600), is(new int[]{1,0,0}));
+    }
+
+    @Test
+    public void itShouldGiveTheHoursMinutesAndSecondsItWillTakeToCatchUp() throws Exception{
+        TortoiseRace race = new TortoiseRace();
+        assertThat(race.getTimeNeededToCatchUp(40, 60, 20), is(new int[]{1, 0, 0}));
+        assertThat(race.getTimeNeededToCatchUp(40, 50, 20), is(new int[]{2, 0, 0}));
+        assertThat(race.getTimeNeededToCatchUp(10, 50, 20), is(new int[]{0, 30, 0}));
+        assertThat(race.getTimeNeededToCatchUp(720, 850, 70), is(new int[]{0, 32, 18}));
     }
 
 
