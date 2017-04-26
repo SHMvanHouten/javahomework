7da0e530ec24365f3e95c75839e4361ab36ac3a2
shmvanhouten <svanhouten@gmail.com>
2017-04-26 16:32:08 +0200

    made it calculate simple multiples of the same book where no discount is given.

--- Practice/src/main/java/com/github/shmvanhouten/PotterKata.java
+++ Practice/src/main/java/com/github/shmvanhouten/PotterKata.java
@@ -1,10 +1,17 @@
 package com.github.shmvanhouten;
 
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 public class PotterKata {
+
     public int getTotalPrice(Map<Integer, Integer> amountOfEachBook) {
-        return 8;
+        BigDecimal totalValue = new BigDecimal(0);
+        for (int bookNumber :
+                amountOfEachBook.keySet()) {
+            totalValue = totalValue.add(new BigDecimal(8.00).multiply(new BigDecimal(amountOfEachBook.get(bookNumber))));
+        }
+        return totalValue.intValue();
     }
 }
--- Practice/src/test/java/com/github/shmvanhouten/PotterKataTest.java
+++ Practice/src/test/java/com/github/shmvanhouten/PotterKataTest.java
@@ -15,7 +15,26 @@ public class PotterKataTest {
     public void itShouldGiveThePrice() throws Exception {
         PotterKata calculator = new PotterKata();
         Map<Integer, Integer> input = new HashMap<>();
+
         input.put(1,1);
-        assertThat(calculator.getTotalPrice(input), is(8));
+        assertThat("one of book 1 == 8 eur",calculator.getTotalPrice(input), is(8));
+
+        input.clear();
+        input.put(2,2);
+        assertThat("two of book 2 == 16 eur",calculator.getTotalPrice(input), is(16));
+
+        input.clear();
+        input.put(3,3);
+        assertThat("three of book 3 == 24 eur",calculator.getTotalPrice(input), is(24));
     }
+
+//    @Test
+//    public void itShouldGiveThePriceWithAppropriateDiscount() throws Exception {
+//        PotterKata calculator = new PotterKata();
+//        Map<Integer, Integer> input = new HashMap<>();
+//
+//        input.put(1,1);
+//        input.put(2,1);
+//        assertThat("one of book 1 and 1 of book 2 == 16 * 0.95", calculator.getTotalPrice(input), is(15.20));
+//    }
 }
