4d167a0808aba7b01ec07beccbdba8fe55c49968
shmvanhouten <svanhouten@gmail.com>
2017-04-26 17:02:31 +0200

    added basic discount calculation and changed the return value to a string which specifies that it is euros

--- Practice/src/main/java/com/github/shmvanhouten/PotterKata.java
+++ Practice/src/main/java/com/github/shmvanhouten/PotterKata.java
@@ -2,16 +2,26 @@ package com.github.shmvanhouten;
 
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Map;
 
 public class PotterKata {
 
-    public int getTotalPrice(Map<Integer, Integer> amountOfEachBook) {
+    public String getTotalPrice(Map<Integer, Integer> amountOfEachBook) {
         BigDecimal totalValue = new BigDecimal(0);
+        int numberOfDifferentTypeBooks = 0;
+
         for (int bookNumber :
                 amountOfEachBook.keySet()) {
             totalValue = totalValue.add(new BigDecimal(8.00).multiply(new BigDecimal(amountOfEachBook.get(bookNumber))));
+            numberOfDifferentTypeBooks++;
         }
-        return totalValue.intValue();
+
+        BigDecimal discount = new BigDecimal(1);
+        for(int i = 1; i<numberOfDifferentTypeBooks; i++){
+            discount = discount.subtract(new BigDecimal(0.05));
+        }
+
+        return (totalValue.multiply(discount)).setScale(2, RoundingMode.CEILING).toString() + " eur";
     }
 }
--- Practice/src/test/java/com/github/shmvanhouten/PotterKataTest.java
+++ Practice/src/test/java/com/github/shmvanhouten/PotterKataTest.java
@@ -3,6 +3,7 @@ package com.github.shmvanhouten;
 import org.junit.Test;
 
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,24 +18,24 @@ public class PotterKataTest {
         Map<Integer, Integer> input = new HashMap<>();
 
         input.put(1,1);
-        assertThat("one of book 1 == 8 eur",calculator.getTotalPrice(input), is(8));
+        assertThat("one of book 1 == 8 eur",calculator.getTotalPrice(input), is("8.00 eur"));
 
         input.clear();
         input.put(2,2);
-        assertThat("two of book 2 == 16 eur",calculator.getTotalPrice(input), is(16));
+        assertThat("two of book 2 == 16 eur",calculator.getTotalPrice(input), is("16.00 eur"));
 
         input.clear();
         input.put(3,3);
-        assertThat("three of book 3 == 24 eur",calculator.getTotalPrice(input), is(24));
+        assertThat("three of book 3 == 24 eur",calculator.getTotalPrice(input), is("24.00 eur"));
     }
 
-//    @Test
-//    public void itShouldGiveThePriceWithAppropriateDiscount() throws Exception {
-//        PotterKata calculator = new PotterKata();
-//        Map<Integer, Integer> input = new HashMap<>();
-//
-//        input.put(1,1);
-//        input.put(2,1);
-//        assertThat("one of book 1 and 1 of book 2 == 16 * 0.95", calculator.getTotalPrice(input), is(15.20));
-//    }
+    @Test
+    public void itShouldGiveThePriceWithAppropriateDiscount() throws Exception {
+        PotterKata calculator = new PotterKata();
+        Map<Integer, Integer> input = new HashMap<>();
+        input.put(1,1);
+        input.put(2,1);
+        String reason = "one of book 1 and 1 of book 2 == 16 * 0.95";
+        assertThat(reason, calculator.getTotalPrice(input), is("15.20 eur"));
+    }
 }
