2a8b2e6dfd3be35750f18e12c2f65a0e9b0e9d78
shmvanhouten <svanhouten@gmail.com>
2017-04-26 17:27:57 +0200

    changes to make the tests represent the kata description better (4 books gives 20% discount instead of 15%) and changed the calculator accordingly

--- Practice/src/main/java/com/github/shmvanhouten/PotterKata.java
+++ Practice/src/main/java/com/github/shmvanhouten/PotterKata.java
@@ -3,9 +3,18 @@ package com.github.shmvanhouten;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PotterKata {
+    Map<Integer, BigDecimal> discountsMap = new HashMap<>();
+    public PotterKata(){
+        discountsMap.put(1, new BigDecimal(1.00));
+        discountsMap.put(2, new BigDecimal(0.95));
+        discountsMap.put(3, new BigDecimal(0.90));
+        discountsMap.put(4, new BigDecimal(0.80));
+        discountsMap.put(5, new BigDecimal(0.75));
+    }
 
     public String getTotalPrice(Map<Integer, Integer> amountOfEachBook) {
         BigDecimal totalValue = new BigDecimal(0);
@@ -17,11 +26,8 @@ public class PotterKata {
             numberOfDifferentTypeBooks++;
         }
 
-        BigDecimal discount = new BigDecimal(1);
-        for(int i = 1; i<numberOfDifferentTypeBooks; i++){
-            discount = discount.subtract(new BigDecimal(0.05));
-        }
+        BigDecimal discount = discountsMap.get(numberOfDifferentTypeBooks);
 
-        return (totalValue.multiply(discount)).setScale(2, RoundingMode.CEILING).toString() + " eur";
+        return (totalValue.multiply(discount)).setScale(2, RoundingMode.HALF_UP).toString() + " eur";
     }
 }
--- Practice/src/test/java/com/github/shmvanhouten/PotterKataTest.java
+++ Practice/src/test/java/com/github/shmvanhouten/PotterKataTest.java
@@ -3,7 +3,6 @@ package com.github.shmvanhouten;
 import org.junit.Test;
 
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,10 +38,18 @@ public class PotterKataTest {
         String reason = "one of book 1 and 1 of book 2 == 16 * 0.95";
         assertThat(reason, calculator.getTotalPrice(input), is("15.20 eur"));
 
-        input.put(3,2);
-        String reason2 = "one of book 1 and 2, two of book 3 == 32 * 0.90";
-        assertThat(reason2, calculator.getTotalPrice(input), is("28.80 eur"));
     }
 
-    
+    @Test
+    public void itShouldGive20PercentDiscountForFourBooks() throws Exception {
+        PotterKata calculator = new PotterKata();
+        Map<Integer, Integer> input = new HashMap<>();
+
+        input.put(1,1);
+        input.put(2,1);
+        input.put(3,1);
+        input.put(4,1);
+        String reason2 = "one of book 1, 2, 3 and 4 == 32 * 0.80";
+        assertThat(reason2, calculator.getTotalPrice(input), is("25.60 eur"));
+    }
 }
