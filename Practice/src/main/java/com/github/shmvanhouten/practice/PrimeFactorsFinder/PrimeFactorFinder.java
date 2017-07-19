17de3485d135929d37d55fb146b743cace5b69f2
ShmvanHouten <svanhouten@gmail.com>
2017-07-19 22:51:59 +0200

    made a primefactorsfinder simple but slightly optimized.

--- Practice/src/main/java/com/github/shmvanhouten/practice/PrimeFactorsFinder/PrimeFactorFinder.java
+++ Practice/src/main/java/com/github/shmvanhouten/practice/PrimeFactorsFinder/PrimeFactorFinder.java
@@ -4,10 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PrimeFactorFinder {
-    public Map<Long, Integer> getPrimeFactors(long i) {
+    public Map<Long, Integer> getPrimeFactors(long number) {
         Map<Long, Integer> factors = new HashMap<>();
 
+        for (long i = 2; i <= number /2; i++) {
+            while(number%i == 0){
+                factors.merge(i,1, Integer::sum);
+                number /= i;
+            }
+        }
 
+        if(number > 1){
+            factors.put(number, 1);
+        }
 
         return factors;
     }
