2cb28629aa02c14f7b033cc1013cd3c4a80495f4
ShmvanHouten <svanhouten@gmail.com>
2017-07-21 22:18:18 +0200

    Implemented getAmountOfBitsToConvert method

--- Practice/src/main/java/com/github/shmvanhouten/practice/binarycomparer/BinaryComparer.java
+++ Practice/src/main/java/com/github/shmvanhouten/practice/binarycomparer/BinaryComparer.java
@@ -1,4 +1,24 @@
 package com.github.shmvanhouten.practice.binarycomparer;
 
+
+import java.util.List;
+
 public class BinaryComparer {
+    private final IntToBinaryConverter binaryConverter;
+
+    public BinaryComparer(IntToBinaryConverter binaryConverter) {
+        this.binaryConverter = binaryConverter;
+    }
+
+    public int getAmountOfBitsToConvert(int firstNumber, int secondNumber) {
+        List<Integer> firstBinaryNumber = binaryConverter.convertIntegerToBinary(firstNumber);
+        List<Integer> secondBinaryNumber = binaryConverter.convertIntegerToBinary(secondNumber);
+        int amountOfBitsToConvert = 0;
+        for (int i = 0; i < firstBinaryNumber.size(); i++) {
+            if(firstBinaryNumber.get(i) != secondBinaryNumber.get(i)){
+                amountOfBitsToConvert++;
+            }
+        }
+        return amountOfBitsToConvert;
+    }
 }
--- Practice/src/test/java/com/github/shmvanhouten/practice/binarycomparer/BinaryComparerTest.java
+++ Practice/src/test/java/com/github/shmvanhouten/practice/binarycomparer/BinaryComparerTest.java
@@ -2,9 +2,28 @@ package com.github.shmvanhouten.practice.binarycomparer;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+
+//Kata: create a method that tells how many bits need to be converted to get from one int to another.
 
 public class BinaryComparerTest {
 
+    @Test
+    public void itShouldCompareTheIntegersBinaryValues() throws Exception {
+        BinaryComparer comparer = new BinaryComparer(new IntToBinaryConverter());
+        assertThat(comparer.getAmountOfBitsToConvert(1,2), is(2));
+    }
 
+    @Test
+    public void itShouldSayTheAmountOfBitsThatNeedToBeChangedIs1() throws Exception {
+        BinaryComparer comparer = new BinaryComparer(new IntToBinaryConverter());
+        assertThat(comparer.getAmountOfBitsToConvert(1,3), is(1));
+    }
+
+    @Test
+    public void itShouldSayTheAmountOfBitsThatNeedToBeChangedIs0() throws Exception {
+        BinaryComparer comparer = new BinaryComparer(new IntToBinaryConverter());
+        assertThat(comparer.getAmountOfBitsToConvert(1,1), is(0));
+    }
 }
