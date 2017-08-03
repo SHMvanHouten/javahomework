375a49473486441f10dc37c67f3f089cdbabf281
Sjoerd <svanhouten@gmail.com>
2017-08-03 20:05:24 +0200

    fizzBuzz

---
+++ Practice/src/main/java/com/github/shmvanhouten/practice/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.github.shmvanhouten.practice.fizzbuzz;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.printUpTo(100);
+    }
+
+    private void printUpTo(int lastNumber) {
+        for (int i = 1; i <= lastNumber; i++) {
+            System.out.println(getPhraseToPrint(i));
+        }
+    }
+
+    private String getPhraseToPrint(int i) {
+        StringBuilder
+        if (i % 3 == 0 && i % 5 == 0) {
+            System.out.println("fizzbuzz");
+        } else if (i % 3 == 0) {
+            System.out.println("fizz");
+        } else if (i % 5 == 0) {
+            System.out.println("buzz");
+        }else{
+            System.out.println(i);
+        }
+    }
+
+}
