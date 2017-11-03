5b4af2fb093d5a23bf4c55b1d81ac9a2e0a2891d
SjhmvanHouten <svanhouten@gmail.com>
2017-11-03 13:56:00 +0100

    added walkthrough of how to implement a lines system to get the answer to question 2 (where do lines cross)

--- Practice/src/main/java/com/github/shmvanhouten/practice/adventofcode/howfariseasterbunnyhq/HowFarIsEasterBunnyHqCalculator.java
+++ Practice/src/main/java/com/github/shmvanhouten/practice/adventofcode/howfariseasterbunnyhq/HowFarIsEasterBunnyHqCalculator.java
@@ -24,7 +24,13 @@ public class HowFarIsEasterBunnyHqCalculator {
 
     private void followInstruction(String instruction) {
         doTurn(instruction);
+        // get direction walker is facing or get both starting coordinates and ending coordinates
+        // get starting point from walker
         takeSteps(instruction);
+        // get ending point from walker
+        // make a line with the starting point and ending point, compare it to stored lines
+        // if lines cross, get point where they cross and break out of the loop, return the coordinates
+        // else (if lines don't cross) add the line to either vertical or horizontal lines map
     }
 
     private void doTurn(String instruction) {
@@ -54,7 +60,7 @@ public class HowFarIsEasterBunnyHqCalculator {
     public static void main(String[] args) {
         HowFarIsEasterBunnyHqCalculator calculator = new HowFarIsEasterBunnyHqCalculator();
 
-        String directions = "L5, R1, L5, L1, R5, R1, R1, L4, L1, L3, R2, R4, L4, L1, L1, R2, R4, R3, L1, R4, L4, L5, L4, R4, L5, R1, R5, L2, R1, R3, L2, L4, L4, R1, L192, R5, R1, R4, L5, L4, R5, L1, L1, R48, R5, R5, L2, R4, R4, R1, R3, L1, L4, L5, R1, L4, L2, L5, R5, L2, R74, R4, L1, R188, R5, L4, L2, R5, R2, L4, R4, R3, R3, R2, R1, L3, L2, L5, L5, L2, L1, R1, R5, R4, L3, R5, L1, L3, R4, L1, L3, L2, R1, R3, R2, R5, L3, L1, L1, R5, L4, L5, R5, R2, L5, R2, L1, L5, L3, L5, L5, L1, R1, L4, L3, L1, R2, R5, L1, L3, R4, R5, L4, L1, R5, L1, R5, R5, R5, R2, R1, R2, L5, L5, L5, R4, L5, L4, L4, R5, L2, R1, R5, L1, L5, R4, L3, R4, L2, R3, R3, R3, L2, L2, L2, L1, L4, R3, L4, L2, R2, R5, L1, R2";
+        String directions = "L3, R1, L4, L1, L2, R4, L3, L3, R2, R3, L5, R1, R3, L4, L1, L2, R2, R1, L4, L4, R2, L5, R3, R2, R1, L1, L2, R2, R2, L1, L1, R2, R1, L3, L5, R4, L3, R3, R3, L5, L190, L4, R4, R51, L4, R5, R5, R2, L1, L3, R1, R4, L3, R1, R3, L5, L4, R2, R5, R2, L1, L5, L1, L1, R78, L3, R2, L3, R5, L2, R2, R4, L1, L4, R1, R185, R3, L4, L1, L1, L3, R4, L4, L1, R5, L5, L1, R5, L1, R2, L5, L2, R4, R3, L2, R3, R1, L3, L5, L4, R3, L2, L4, L5, L4, R1, L1, R5, L2, R4, R2, R3, L1, L1, L4, L3, R4, L3, L5, R2, L5, L1, L1, R2, R3, L5, L3, L2, L1, L4, R4, R4, L2, R3, R1, L2, R1, L2, L2, R3, R3, L1, R4, L5, L3, R4, R4, R1, L2, L5, L3, R1, R4, L2, R5, R4, R2, L5, L3, R4, R1, L1, R5, L3, R1, R5, L2, R1, L5, L2, R2, L2, L3, R3, R3, R1";
         System.out.println(calculator.findDistanceToHq(directions));
 
     }
